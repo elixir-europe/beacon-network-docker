10d2cb9ff8bbfd41911185fad8e31cb0bf58630c
dmitry <dmitry.repchevski@bsc.es>
2023-12-12 09:59:55 +0000

    remove docker recipe (use latest image)

--- postgresql/init.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER beacon WITH ENCRYPTED PASSWORD 'beacon';
-GRANT ALL ON SCHEMA public TO beacon;
-CREATE DATABASE "beacon-network" OWNER beacon;
--- wildfly/deploy.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-# download beacon server for the deployment
-curl https://inb.bsc.es/maven/es/bsc/inb/ga4gh/beacon-network-v2/0.0.8/beacon-network-v2-0.0.8.war > /opt/jboss/wildfly/standalone/deployments/beacon-network-v2-0.0.8.war
-
-/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0
